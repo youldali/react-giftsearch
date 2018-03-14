edb846c01723502a4bb4822d0db26881ece257a0
youldali <yannis.ouldali@smartbox.com>
2018-03-14 19:24:26 +0000

    refactor storage IDB

--- src/modules/gift-search/helpers/storage.js
+++ src/modules/gift-search/helpers/storage.js
@@ -4,7 +4,9 @@ import type { FieldsToIndexByUniverse, FieldsToIndex, IndexConfig } from 'module
 import type { FilterCriteria, FilterGroup } from 'modules/gift-search/filter.config';
 import type { Operator, FilterOperand } from 'modules/actions/types';
 
-import { curry, mapObjIndexed } from 'ramda';
+import { curry, mapObjIndexed, mergeAll } from 'ramda';
+
+type FilterMatchingIdList = {[string]: [number]};
 
 const _createUniverseStore = (db: IDBDatabase, fieldsToIndex: FieldsToIndex, universe: string) => {
     const 
@@ -42,27 +44,28 @@ const _addDataToUniverse = (db: IDBDatabase, universe: string, data: Object[]): 
     data.forEach( row => objectStore.add(row) );
     return new Promise((resolve, reject) => {
         transaction.oncomplete = () => resolve();
-        transaction.onerror = () => reject(`error inserting data for universe ${universe}: ${transaction.error.message}`);
+        transaction.onerror = () => reject(`error inserting data for universe ${universe}:`);
     });
 };
 export const addDataToUniverse = curry(_addDataToUniverse);
 
 
-const _getFilterNumber = (db: IDBDatabase, universe: string, filterCriteria: FilterCriteria, filterOperandFallback: FilterOperand) => {
+const _getFilterMatchingIdList = (db: IDBDatabase, universe: string, filterCriteria: FilterCriteria, filterOperandFallback: FilterOperand): Promise<FilterMatchingIdList> => {
+    const operatorMultiValue = ['isIncluded', 'hasOneInCommon'];
     const 
         {field, operator, operand} = filterCriteria,
         nonNullOperand = operand || filterOperandFallback,
-        mOperand = Array.isArray(nonNullOperand) ? nonNullOperand : [nonNullOperand];
+        mOperand = operatorMultiValue.includes(operator) ? nonNullOperand : [nonNullOperand];
 
-    const result = {};
-    mOperand.forEach(operandValue => {
-        const keyRange = getKeyRangeMatchingOperator(operator, operandValue);
-        result[operandValue] = getFieldIds(db, universe, field, keyRange);
-    });
-    
-    return result;
+    //$FlowFixMe
+    const operandNumberListPromise = mOperand.map( operand => 
+        getFieldIds(db, universe, field, getKeyRangeMatchingOperator(operator, operand))
+        .then(matchingIdList => ( {[operand.toString()]: matchingIdList} )) 
+    );
+
+    return Promise.all(operandNumberListPromise).then((operandNumberList) => mergeAll(operandNumberList));
 };
-export const getFilterNumber = curry(_getFilterNumber);
+export const getFilterMatchingIdList = curry(_getFilterMatchingIdList);
 
 
 const _getFieldIds = (db: IDBDatabase, storeName: string, indexName: string, keyRange: IDBKeyRange) => {
@@ -73,7 +76,7 @@ const _getFieldIds = (db: IDBDatabase, storeName: string, indexName: string, key
         request: IDBRequest = index.getAllKeys(keyRange);
 
     return new Promise((resolve, reject) => {
-        request.onsuccess = () => Promise.resolve(request.result);
+        request.onsuccess = () => resolve(request.result);
         request.onerror = () => reject('error fetching data: ' + request.error.message);
     });
 };
@@ -123,8 +126,32 @@ const _iterateOverBoxesInUniverse = (db: IDBDatabase, universe: string, callBack
     };
 
     return new Promise((resolve, reject) => {
-        transaction.oncomplete = () => Promise.resolve();
+        transaction.oncomplete = () => resolve();
         transaction.onerror = () => reject('error fetching data: ' + transaction.error.message);
     });
 };
 export const iterateOverBoxesInUniverse = curry(_iterateOverBoxesInUniverse);
+
+const _getAllUniqueKeysForIndex = (db: IDBDatabase, universe: string, field: string) => {
+    const 
+        transaction = db.transaction(storeName, 'readonly'),
+        objectStore = transaction.objectStore(storeName),
+        index = objectStore.index(indexName),
+        request: IDBRequest = index.openCursor(null, 'nextunique');
+
+
+    const keyList = [];
+    request.onsuccess = function(event) {
+        const cursor = event.target.result;
+        if(cursor) {
+            keyList.push(cursor.value);
+            cursor.continue();
+        }
+    };
+
+    return new Promise((resolve, reject) => {
+        transaction.oncomplete = () => resolve(keyList);
+        transaction.onerror = () => reject('error fetching key list');
+    });
+};
+export const getAllUniqueKeysForIndex = curry(_getAllUniqueKeysForIndex);
